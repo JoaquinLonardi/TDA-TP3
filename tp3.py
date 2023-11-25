aba18507de4455c69964a6efd234156108e15d44
Joaquín Lonardi <jlonardi@fi.uba.ar>
2023-11-24 23:42:13 -0300

    Preparo entrega

--- tp3.py
+++ tp3.py
@@ -1,8 +1,9 @@
 import sys
-from algoritmo import hitting_set
+from backtracking import hitting_set
 from programacion_lineal import hitting_set_pl
 from programacion_lineal import hitting_set_aprox
 import csv
+
 
 def leer_archivo(filepath):
     conjuntos_periodistas = []
@@ -18,26 +19,26 @@ def leer_archivo(filepath):
     return conjuntos_periodistas, conjunto_universo
 
 
-if len(sys.argv) == 1:
-    print('ERROR: Falta especificar el archivo como argumento.')
-    sys.exit()
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('ERROR: Falta especificar el archivo como argumento.')
+        sys.exit()
 
+    if len(sys.argv) == 2:
+        print(f"ERROR: Falta especificar el flag de función")
+        sys.exit()
 
-if len(sys.argv) == 2:
-    print(f"ERROR: Falta especificar el flag de función")
-    sys.exit()
+    S, U = leer_archivo(sys.argv[1])
+    flag = sys.argv[2]
 
-S, U = leer_archivo(sys.argv[1])
-flag = sys.argv[2]
+    if flag == '-bt':
+        solucion = hitting_set(S, U)
+    elif flag == '-pl':
+        solucion = hitting_set_pl(S, U)
+    elif flag == '-aprox':
+        solucion = hitting_set_aprox(S, U)
+    else:
+        print(f"ERROR: Flag incorrecto. Las opciones posibles son {['-bt', '-pl', '-aprox']}")
+        sys.exit()
 
-if flag == '-bt':
-    solucion = hitting_set(S, U)
-elif flag == '-pl':
-    solucion = hitting_set_pl(S, U)
-elif flag == '-aprox':
-    solucion = hitting_set_aprox(S, U)
-else:
-    print(f"ERROR: Flag incorrecto. Las opciones posibles son {['-bt', '-pl', '-aprox']}")
-    sys.exit()
-
-print(f"La mínima cantidad de jugadores que puede llevar Scaloni para contentar al periodismo es: {solucion}")
+    print(f"La mínima cantidad de jugadores que puede llevar Scaloni para contentar al periodismo es: {solucion}")
