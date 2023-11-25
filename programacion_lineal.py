89820705bbd38ae241ee0e9975f4ad49d9adbd29
Joaquín Lonardi <jlonardi@fi.uba.ar>
2023-11-24 23:25:18 -0300

    ultimos cambios

--- programacion_lineal.py
+++ programacion_lineal.py
@@ -41,32 +41,27 @@ def hitting_set_bilardo(S, U=None):
             U.update(conjunto)
 
     variables_elementos = pulp.LpVariable.dicts("elemento", U, lowBound=0, upBound=1)
-    hitting_set = pulp.LpProblem("Hitting Set Problem", pulp.LpMinimize)
+    hitting_set_problem = pulp.LpProblem("Hitting Set Problem", pulp.LpMinimize)
 
     # funcion a optimizar
-    hitting_set += pulp.lpSum(variables_elementos)
+    hitting_set_problem += pulp.lpSum(variables_elementos)
 
     max_cardinalidad_conjunto = max([len(conjunto) for conjunto in S])
 
-    # for var in variables_elementos:
-    #     var == 1 if
     for conjunto in S:
-        # La suma de las variables -que van a ser 1 o 0- para cada conjunto debe ser mayor a 1. Es decir, debe tener
-        # al menos un elemento
-        hitting_set += pulp.lpSum(
+        hitting_set_problem += pulp.lpSum(
             variables_elementos[elemento] for elemento in conjunto
         ) >= 1
 
-    hitting_set.solve()
-    valores = [elemento.value() for elemento in hitting_set.variables()]
+    hitting_set_problem.solve()
 
     solucion = 0
-    for elemento in hitting_set.variables():
+    for elemento in hitting_set_problem.variables():
         valor = elemento.value()
         if valor >= 1/max_cardinalidad_conjunto:
             solucion += 1
 
 
-
+    print(f"Solución que me dio así nomás: {sum([elemento.value() for elemento in hitting_set_problem.variables()])}. Solución que me dio redondeando: {solucion}. Solución verdaderoa: {hitting_set(S)}. MAx cardinal {max_cardinalidad_conjunto}")
     return solucion, max_cardinalidad_conjunto
     # return sum([x.value() for x in hitting_set.variables()])
--- tp3.py
+++ tp3.py
@@ -1,18 +1,43 @@
 import sys
-from algoritmo import algoritmo_backtracking
+from algoritmo import hitting_set
+from programacion_lineal import hitting_set_pl
+from programacion_lineal import hitting_set_aprox
 import csv
 
-def leer_archivo(filename):
-    try:
-        with open(filename, newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            for x in reader:
-                print(x)
-    except OSError:
-        print('ERROR: Archivo no encontrado.')
-        sys.exit()
+def leer_archivo(filepath):
+    conjuntos_periodistas = []
+    with open(filepath, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for x in reader:
+            conjuntos_periodistas.append(set(x))
+
+    conjunto_universo = set()
+    for conjunto in conjuntos_periodistas:
+        conjunto_universo.update(conjunto)
+
+    return conjuntos_periodistas, conjunto_universo
 
 
-if len(sys.argv) == 1:
+if not sys.argv[1]:
     print('ERROR: Falta especificar el archivo como argumento.')
     sys.exit()
+
+
+if not sys.argv[2]:
+    print(f"ERROR: Falta especificar el flag de función")
+    sys.exit()
+
+S, U = leer_archivo(sys.argv[1])
+flag = sys.argv[2]
+
+if flag == '-bt':
+    solucion = hitting_set(S, U)
+if flag == '-pl':
+    solucion = hitting_set_pl(S, U)
+if flag == '-aprox':
+    solucion = hitting_set_aprox(S, U)
+else:
+    print(f"ERROR: Flag incorrecto. Las opciones posibles son {['-bt', '-pl', '-aprox']}")
+    sys.exit()
+
+print(f"La mínima cantidad de jugadores que puede llevar Scaloni para contentar al periodismo es: {solucion}")
